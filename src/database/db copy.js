fa7972b5118c23c7a4a072c2aa77876775cdb573
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2022-11-09 11:54:03 -0300

    docker local db

--- src/database/db copy.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-async function connectDatabase() {
-  await mongoose.connect("mongodb://localhost/depoiment");
-}
-
-export default connectDatabase;
--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,5 @@
 import mongoose from "mongoose";
 
-// async function connectDatabase() {
-//   await mongoose.connect(
-//     "mongodb://Tiago:cacli200900@ac-wzei49q-shard-00-00.dym7xwx.mongodb.net:27017,ac-wzei49q-shard-00-01.dym7xwx.mongodb.net:27017,ac-wzei49q-shard-00-02.dym7xwx.mongodb.net:27017/?ssl=true&replicaSet=atlas-9tdgcf-shard-0&authSource=admin&retryWrites=true&w=majority"
-//   );
-// }
-
 async function connectDatabase() {
   await mongoose.connect("mongodb://localhost/depoiment");
 }
